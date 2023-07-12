6baad6e4801dd3e1a7ea01a13f5d55b3d466aef6
HuaQitian519 <107608976+HuaQitian519@users.noreply.github.com>
2023-07-12 19:45:07 +0800

    Movie_manager.cpp

--- Movie_manager/Movie_manager.cpp
+++ Movie_manager/Movie_manager.cpp
@@ -1,12 +1,13 @@
-﻿
+
 #include <stdio.h>
 #include <stdlib.h>
 struct movie_info {
-    int id;
+
     char name[20];
     long long date;
     int price;
-    int seat[5][8] = {0};
+    int remain_ticket = 40;
+
 };
 struct admin_info {
     int admin_id;
@@ -22,6 +23,7 @@ struct order_info {
     long long date;
     int user_id;
 };
+movie_info movies[50];
 void add_admin();
 void add_user();
 void login_admin();
@@ -42,8 +44,32 @@ void check_ticket();
 void display_list();
 void back();
 
+//user部分功能
+void buy_ticket();
 int main()
 {
-   
+    buy_ticket();
 }
-
+void buy_ticket()
+{
+    FILE* fp = fopen("movie_info.txt", "r");
+    if (fp == NULL) {
+        printf("电影信息加载失败！\n");
+        char ch;
+        while ((ch = getchar()) != '\n');
+        getchar();
+        return;
+    }
+    int num_movies = 0;
+    while ((fscanf(fp, "%s %lld %d", &movies[num_movies].name, &movies[num_movies].date, &movies[num_movies].price))== 3) {  
+        num_movies++;
+    }
+    fclose(fp);
+    printf("片名\t时间\t票价\t剩余座位\n");
+    for (int i = 0; i < num_movies; i++) {
+        printf("%s %lld %d\n", movies[i].name, movies[i].date, movies[i].price);
+    }
+    char ch;
+    while ((ch = getchar()) != '\n');
+    getchar();
+}
